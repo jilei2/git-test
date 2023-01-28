a65081f432fcef856e6955c2f89fee49a73257fa
jilei <jilei@qq.com>
2023-01-28 14:25:19 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author 季 雷
+ * @version 1.0
+ * @Date 2023/01/28/14:22
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
